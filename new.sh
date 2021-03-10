1e21b7b9268c5e8bc6e66ea667400def1bed0810
robert-clayton <41345902+robert-clayton@users.noreply.github.com>
2021-03-10 10:06:06 -0800

    Add obs, switch tablet bindings

--- new.sh
+++ new.sh
@@ -6,7 +6,7 @@ sudo dnf update
 wget https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-33.noarch.rpm
 sudo rpm -i rpmfusion*.rpm
 rm rpmfusion*.rpm
-sudo dnf install ffmpeg -y
+sudo dnf install -y ffmpeg obs-studio
 
 # # Set fish as default shell
 # sudo dnf install fish util-linux-user -y
@@ -24,3 +24,16 @@ sudo find /etc/openvpn/*.conf -type f -exec sed -i 's/auth-user-pass/auth-user-p
 sudo wget https://www.perfect-privacy.com/downloads/updown.sh
 sudo chmod +x updown.sh
 sudo find /etc/openvpn/*.conf -type f -exec sed -i 's/update-resolv-conf/updown.sh/g' {} \;
+
+
+# Get OBS going
+sudo dnf install -y ffmpeg obs-studio
+git clone https://github.com/umlaeute/v4l2loopback /mnt/Ziru/Repos/v4l2loopback
+cd /mnt/Ziru/Repos/v4l2loopback
+make && sudo make install
+sudo depmod -a
+
+# Instagram LUTs
+# https://www.dropbox.com/sh/hwnchnz2u4sgp4e/AACEluoupZbFrn7yU72IzeRda?dl=0
+# OBS LUTs
+# https://drive.google.com/uc?export=download&id=1Y3cqidw8i0D7WaZapw_ncQlSxJ44273g
